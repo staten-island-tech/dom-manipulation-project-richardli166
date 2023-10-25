b4b157f7c70cd47a0a35b7dec06954459dda12f8
richardli166 <water.richard.li@gmail.com>
2023-10-25 09:24:41 -0400

    e5hyaewy

--- app.js
+++ app.js
@@ -3,13 +3,19 @@ const DOMSelectors = {
     firstName: document.querySelector(".first-name"),
     h2s: document.querySelectorAll("h2"),
     button: document.getElementById("button"),
+    
 };
 function makeAlbum(){
     
 }
 function addCard(Album){
 }
+function remove(){
+
+}
 DOMSelectors.form.addEventListener("submit", function(event){
+    addCard;
+    makeAlbum;
     event.preventDefault();
     document.querySelector(".gallery").insertAdjacentHTML()
         //const Album = makeAlbum()
@@ -17,4 +23,3 @@ DOMSelectors.form.addEventListener("submit", function(event){
         //clearFields()
         //addRemoveButtons
     });
-DOMSelectors
